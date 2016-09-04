6c9a5ac59f084ac1a7cf5210df449875b1caa983
fy <guiguoershao@163.com>
2016-09-04 16:25:25 +0800

    报表8

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -11,6 +11,14 @@ class IndexController extends AdminController
 
     public function main()
     {
+        //  资金余额
+
+        //  客户欠款    供应商欠款
+
+        //  本月销售收入  毛利
+
+        //  本月采购金额
+
         $this->display();
     }
 
